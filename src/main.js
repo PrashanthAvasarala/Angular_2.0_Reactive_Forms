e499bf24765eb97783df9179377a1a584387675f
avasarala786 <prashanth.avasarala@gmail.com>
2017-10-06 01:45:00 -0400

    ok

---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// tslint:disable:no-unused-variable
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var demo_module_1 = require("./app/demo.module"); // demo picker
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(demo_module_1.DemoModule); // (AppModule);
+//# sourceMappingURL=main.js.map
